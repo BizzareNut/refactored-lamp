463b6a632825678ffe6940c5bb4aab09f57180e7
BizzareNut <mqq170066@outlook.com>
2023-02-21 10:33:45 +0000

    test and just add some chinese

--- app/events/OtherClicked.java
+++ app/events/OtherClicked.java
@@ -8,7 +8,7 @@ import structures.GameState;
 /**
  * Indicates that the user has clicked an object on the game canvas, in this case
  * somewhere that is not on a card tile or the end-turn button.
- * 
+ * * 表示用户在游戏画布上点击了一个物体，在这种情况下是在卡牌或结束转动按钮上的某个地方
  * { 
  *   messageType = “otherClicked”
  * }
--- app/events/TileClicked.java
+++ app/events/TileClicked.java
@@ -10,6 +10,9 @@ import structures.GameState;
  * Indicates that the user has clicked an object on the game canvas, in this case a tile.
  * The event returns the x (horizontal) and y (vertical) indices of the tile that was
  * clicked. Tile indices start at 1.
+ * 表示用户已经点击了游戏画布上的一个物体，在这种情况下是一个瓦片。
+ * 该事件返回被点击的瓦片的x（水平）和y（垂直）指数。
+ * 瓷砖指数从1开始。
  * 
  * { 
  *   messageType = “tileClicked”
